304cf9310388150a915ffea3d2030f9a8d83365d
yuvinokurov <yuvinokurov@ut-it.ru>
2024-02-12 15:02:36 +0300

    change login/logout with persist

--- lib/store.ts
+++ lib/store.ts
@@ -1,17 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { EnhancedStore, StoreEnhancer, ThunkDispatch, Tuple, UnknownAction, combineReducers, configureStore } from '@reduxjs/toolkit';
 import themeReducer from './features/themeSlice';
-import { authReducer } from './features/authSlice';
+import { IAuthState, authReducer } from './features/authSlice';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { persistStore } from "redux-persist";
+
+const authPersistConfig = {
+  key: "auth",
+  storage: storage,
+  whitelist: ["authState"],
+};
+
+const themePersistConfig = {
+  key: "theme",
+  storage: storage,
+  whitelist: ["themeState"],
+};
+
+const rootReducer = combineReducers({
+  auth: persistReducer(authPersistConfig, authReducer),
+  theme: persistReducer(themePersistConfig, themeReducer)
+});
 
 export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      theme: themeReducer,
-      auth: authReducer,
-    },
+  const store = configureStore({
+    reducer: rootReducer,
     // middleware: (getDefaultMiddleware) => {
     //   getDefaultMiddleware({ serializableCheck: false })
     // }
   });
+
+  persistStore(store);
+
+  return store;
 };
 
 // Infer the type of makeStore
