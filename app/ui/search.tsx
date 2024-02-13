02184b424ba5247466fa6efe785e95dd02664a68
yuvinokurov <yuvinokurov@ut-it.ru>
2024-02-13 15:38:17 +0300

    search params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,19 +5,21 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const  searchParams  = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   const handleSearch = (term: string) => {
     console.log('term', term);
     const params = new URLSearchParams(searchParams);
-    console.log('params', params);
-    const pathname = usePathname();
-    const { replace } = useRouter();
+
 
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
+    console.log('params', params);
+
     replace(`${pathname}?${params.toString()}`)
   }
 
@@ -29,7 +31,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => handleSearch(e.target.value)}
+        onChange={(e) => {
+          handleSearch(e.target.value)
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
